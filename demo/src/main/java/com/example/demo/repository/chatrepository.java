b952a4ea8efd08665654c134f2701a9711f5a093
santhoshkumarcbe <santhoshkumarcbe35@gmail.com>
2023-12-14 10:13:02 +0530

    spring-boot

--- demo/src/main/java/com/example/demo/repository/chatrepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import java.util.*;
-import org.springframework.data.repository.CrudRepository;
-import com.example.demo.model.chat;
-
-public interface chatrepository extends CrudRepository<chat, String> {
-	List<chat> findByVanishMode(boolean val);
-	List<chat> findByVanishModeAndChatId(boolean val,String cid);
-}
