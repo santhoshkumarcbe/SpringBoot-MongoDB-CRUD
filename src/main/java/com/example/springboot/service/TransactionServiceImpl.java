5e2b9247d9e0ffa6eb727fdfda7ca85332c97053
santhoshkumarcbe <santhoshkumarcbe35@gmail.com>
2023-12-16 10:58:33 +0530

    transaction

--- src/main/java/com/example/springboot/service/TransactionServiceImpl.java
+++ src/main/java/com/example/springboot/service/TransactionServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.springboot.entity.Supplier;
 import com.example.springboot.entity.Transaction;
 import com.example.springboot.repository.SupplierRepository;
 import com.example.springboot.repository.TransactionRepository;
@@ -19,20 +18,39 @@ public class TransactionServiceImpl implements TransactionService {
     private SupplierRepository supplierRepository;
     @Override
     public boolean saveTransaction(Transaction transaction) {
+        int transactionLimit=1000;
         boolean isSupplierId=supplierRepository.existsBySupplierId(transaction.getSupplierId());
         boolean isSupplierName=supplierRepository.existsBySupplierName(transaction.getSupplierName());
         boolean isTransactionSupplierId=transactionRepository.existsBySupplierId(transaction.getSupplierId());
         boolean isTransactionSupplierName=transactionRepository.existsBySupplierName(transaction.getSupplierName());
         if (isSupplierId  && isSupplierName && !isTransactionSupplierId && !isTransactionSupplierName) {
-            Supplier supplier=supplierRepository.findBySupplierId(transaction.getSupplierId());
             ArrayList<Integer> transactionList=new ArrayList<>();
             transactionList.add(transaction.getPrice());
             transaction.setTransactionList(transactionList);
-            transaction.setTotalPrice(transaction.getPrice());
+            if (transaction.getPrice() <= transactionLimit) {
+                transaction.setTotalPrice(transaction.getPrice());
+            }
+            transactionRepository.save(transaction);
+            return true;
         }
-        Transaction ExsitingTransaction=transactionRepository.findBySupplierId(transaction.getSupplierId());
-
-        transactionRepository.save(transaction);
+        else if (isSupplierId  && isSupplierName && isTransactionSupplierId && isTransactionSupplierName) {
+            Transaction ExsitingTransaction=transactionRepository.findBySupplierId(transaction.getSupplierId());
+            int totalPrice=ExsitingTransaction.getTotalPrice();
+            totalPrice+=transaction.getPrice();
+            if (totalPrice < transactionLimit) {
+                ArrayList<Integer> transactionList=ExsitingTransaction.getTransactionList();
+                transactionList.add(transaction.getPrice());
+                ExsitingTransaction.setTransactionList(transactionList);
+                ExsitingTransaction.setTotalPrice(totalPrice);
+                transactionRepository.save(ExsitingTransaction);
+                return true;
+            }
+            else{
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
     
 }
